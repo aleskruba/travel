5e918f66b7f583ebee9530828fbc2f90dd418602
Ales <aleskruba@seznam.cz>
2024-06-16 21:43:34 +0200

    socket uri II

--- src/config/config.js
+++ src/config/config.js
@@ -2,9 +2,9 @@
 
 const BASE_URL = 'https://travelbackend-374j.onrender.com/api';
 export default BASE_URL;
-export const SOCKET_URL = 'https://travelsocket.onrender.com';
+//export const SOCKET_URL = 'https://travelsocket.onrender.com';
 
-//export const SOCKET_URL = 'http://localhost:3001';
+export const SOCKET_URL = 'http://localhost:3001';
 export const config = {
     headers: {
       'Content-Type': 'application/json',
